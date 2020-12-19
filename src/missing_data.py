998d91be37eea31ab24ec0a786fbf744c93e4e0d
Kyohei Uto <h27.kubb@gmail.com>
2020-12-19 16:04:48 +0900

    resampleの欠損データを修正

--- src/missing_data.py
+++ src/missing_data.py
@@ -42,14 +42,40 @@ for species_id in train_tp.species_id.unique():
     species_dir.mkdir(parents=True, exist_ok=True)
 
 
-for recording_id, species_id in tqdm(train_audio_infos):
-    species_dir = TRAIN_RESAMPLED_DIR / str(species_id)
-    y, _ = librosa.load(TRAIN_AUDIO_DIR / (recording_id + ext), sr=TARGET_SR, mono=True, res_type="kaiser_fast")
-    sf.write(species_dir / (recording_id + re_ext), y, samplerate=TARGET_SR)
+# train missing data processing
+train_resample_audio_infos = list(TRAIN_RESAMPLED_DIR.glob("*/*.wav"))
+missing_train_audio_id = []
 
-for audio_dir in tqdm(test_audio_infos):
-    y, _ = librosa.load(audio_dir, sr=TARGET_SR, mono=True, res_type="kaiser_fast")
-    sf.write(TEST_RESAMPLED_DIR / audio_dir.name.replace(ext, re_ext), y, samplerate=TARGET_SR) 
+for train_resample_dir in train_resample_audio_infos:
+    y, _ = librosa.load(train_resample_dir, sr=TARGET_SR, mono=True, res_type="kaiser_fast")
+
+    # 1s以下のものは破損しているとみなす
+    length = TARGET_SR * 1
+    if len(y) < length:
+        print(train_resample_dir.stem)
+        missing_train_audio_id.append(train_resample_dir.stem)
+
+for recording_id, species_id in tqdm(train_audio_infos):
+    if recording_id in missing_train_audio_id:
+        y, _ = librosa.load(TRAIN_AUDIO_DIR / (recording_id + ext), sr=TARGET_SR, mono=True, res_type="kaiser_fast")
+        sf.write(TRAIN_RESAMPLED_DIR / str(species_id) / (recording_id + re_ext), y, samplerate=TARGET_SR)
+
+
+# test missing data processing
+# test_resample_audio_infos = list(TEST_RESAMPLED_DIR.glob("*.wav"))
+# missing_test_audio_id = []
+# for test_resample_dir in test_resample_audio_infos:
+#     y, _ = librosa.load(test_resample_dir, sr=TARGET_SR, mono=True, res_type="kaiser_fast")
+#     length = TARGET_SR * 60
+#     if len(y) != length:
+#         print(test_resample_dir.stem)
+#         missing_test_audio_id.append(test_resample_dir.stem)
+
+# for audio_dir in test_audio_infos:
+#     if audio_dir.stem in missing_test_audio_id:
+#         y, _ = librosa.load(audio_dir, sr=TARGET_SR, mono=True, res_type="kaiser_fast")
+#         sf.write(TEST_RESAMPLED_DIR / audio_dir.name.replace(ext, re_ext), y, samplerate=TARGET_SR) 
+
 
 # add information of resampled audios to train.csv
 train["resampled_filename"] = train["recording_id"].map(lambda x: x + ".wav")
