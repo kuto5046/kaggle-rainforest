43210501768a40b8d19b3d2f98fa3af115f00871
Kyohei Uto <h27.kubb@gmail.com>
2020-12-22 07:24:02 +0000

    fix

--- src/dataset.py
+++ src/dataset.py
@@ -37,8 +37,7 @@ class SpectrogramDataset(data.Dataset):
         sample = self.df.loc[idx, :]
         recording_id = sample["recording_id"]
         main_species_id = sample["species_id"]
-        ext = "wav"
-        y, sr = sf.read(self.datadir / f"{recording_id}.{ext}")  # for resample
+        y, sr = sf.read(self.datadir / main_species_id / f"{recording_id}.wav")  # for resample
         # y, sr = sf.read(self.datadir / f"{recording_id}.flac")  # for default
         effective_length = sr * PERIOD
 
@@ -185,8 +184,7 @@ class SpectrogramValDataset(data.Dataset):
         main_species_id = sample["species_id"]
 
         total_time = 60  # 音声を全て60sに揃える
-        ext = "wav"
-        y, sr = sf.read(self.datadir / f"{recording_id}.{ext}")  # for resample
+        y, sr = sf.read(self.datadir / main_species_id / f"{recording_id}.wav")  # for resample
         # y, sr = sf.read(self.datadir / f"{recording_id}.flac")  # for default
 
         if self.waveform_transforms:
