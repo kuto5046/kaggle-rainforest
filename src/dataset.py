3a493c6f53dfcd04cb912070b8997419681b56df
Kyohei Uto <h27.kubb@gmail.com>
2021-01-31 20:06:18 +0900

    fix

--- src/dataset.py
+++ src/dataset.py
@@ -55,7 +55,7 @@ class NpzDataset(data.Dataset):
         image = np.load(self.datadir / f"{recording_id}.npy")  
         image = mono_to_color(image)
         image = np.moveaxis(image, 2, 0)
-        image = (image - image.min())/(image.max()-image.min()).astype(float32)  # norm 0~1
+        image = (image - image.min())/(image.max()-image.min()).astype("float32")  # norm 0~1
         
         if self.phase == 'train':
             p = random.random()
