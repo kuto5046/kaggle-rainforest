2d3d5be213e5499777da92f29cfe699b6d0fa9d5
Kyohei Uto <h27.kubb@gmail.com>
2021-02-05 10:37:49 +0900

    change loss

--- src/criterion.py
+++ src/criterion.py
@@ -48,23 +48,27 @@ class FocalLoss(nn.Module):
         input = inputs[self.output_key]
         target = target.float()
     
-        posi_mask = (target == 1).float()
+        # posi_mask = (target == 1).float()
+
+        posi_mask = (target >= 0).float()  
         nega_mask = (target == -1).float()  # (20, 24)
         
         # validの場合view, maxで分割したデータを１つのデータとして集約する必要がある
         if phase == 'valid':
             input = C.split2one(input, target)
         
-        # posi_y = torch.where(target > 0., 1., 0.).to('cuda')
-        posi_y = torch.ones(input.shape).to('cuda')
+        posi_y = torch.where(target > 0., 1., 0.).to('cuda')  # -1を0にする
+        # posi_y = torch.ones(input.shape).to('cuda')
         nega_y = torch.zeros(input.shape).to('cuda')  # dummy
 
         posi_loss = self.posi_loss(input, posi_y)
         nega_loss = self.nega_loss(input, nega_y)  # 全て負例と見做してloss計算
         probas = input.sigmoid()
-        focal_pw = (1. - probas)**self.gamma
-        posi_loss = (posi_loss * posi_mask * focal_pw).sum()
-        nega_loss = (nega_loss * nega_mask).sum()  # ラベルのついているクラスのみlossを残す
+        # focal_pw = (1. - probas)**self.gamma
+        # posi_loss = (posi_loss * posi_mask * focal_pw).sum()
+        posi_loss = torch.where(target >= 0.5, (1. - probas)**self.gamma * posi_loss, probas**self.gamma * posi_loss)
+        nega_loss = (nega_loss * nega_mask).sum() * 0 # ラベルのついているクラスのみlossを残す
+
         # pos_w = 0 if posi_mask.sum() == 0 else 1/posi_mask.sum()
         # nega_w = 0 if nega_mask.sum() == 0 else 1/nega_mask.sum()
         return posi_loss, nega_loss
