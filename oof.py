167889f8a62e920bf633884a1abe4698a9af0765
Kyohei Uto <h27.kubb@gmail.com>
2021-02-10 14:45:09 +0900

    fix

--- oof.py
+++ oof.py
@@ -123,7 +123,7 @@ def main():
     warnings.filterwarnings('ignore')
 
     # config
-    config_filename = 'EfficientNetSED002.yaml'
+    config_filename = 'ResNeStSED002.yaml'
     config = utils.load_config(f"configs/{config_filename}")
     global_config = config['globals']
     hash_value = utils.get_hash(config)  # get git hash value(short ver.)
